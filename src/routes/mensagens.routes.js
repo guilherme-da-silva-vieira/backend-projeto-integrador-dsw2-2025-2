bbd566619429a470de0d4fa1843a053c4539b7fa
Guilherme da Silva Vieira <guilherme_silva_vieira@proton.me>
2025-12-05 21:22:14 -0300

    Autenticação para GET de mensagens

--- src/routes/mensagens.routes.js
+++ src/routes/mensagens.routes.js
@@ -5,6 +5,7 @@ import { pool } from "../bd/db.js";
 import multer from "multer"; // import do multer
 import path from "path";     // import do path
 import fs from "fs";         // import do fs
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
@@ -13,11 +14,11 @@ const uploadDir = path.resolve('uploads');
 fs.mkdirSync(uploadDir, { recursive: true });
 
 const storage = multer.diskStorage({
-  destination: (_req, _file, cb) => cb(null, uploadDir),
-  filename: (_req, file, cb) => {
-    const ext = path.extname(file.originalname).toLowerCase();
-    cb(null, `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`);
-  }
+    destination: (_req, _file, cb) => cb(null, uploadDir),
+    filename: (_req, file, cb) => {
+        const ext = path.extname(file.originalname).toLowerCase();
+        cb(null, `${Date.now()}-${Math.round(Math.random() * 1e9)}${ext}`);
+    }
 });
 const upload = multer({ storage });
 
@@ -30,11 +31,43 @@ const upload = multer({ storage });
 
 //GET /api/mensagens
 router.get("/", async (_req, res) => {
+    const header = _req.headers["authorization"];
+    if(!header || !header.startsWith("Bearer ")) return res.status(401).json({erro: "token ausente"})
+    const token = header.slice(7);
+    const decode = jwt.decode(token);
     try {
-        const { rows } = await pool.query(`SELECT * FROM "Mensagens" ORDER BY "id" DESC`);
-        res.json(rows);
-    } catch {
+        const { rows} = await pool.query(`SELECT * FROM "Mensagens" WHERE "Usuarios_id" = $1 OR "Usuarios_id_destinatario"= $1 ORDER BY "id" DESC;`, [decode.sub]);
+        return res.json(rows);
+    } catch (erro){
+        console.log(erro);
         res.status(500).json({ erro: "erro interno" });
+    }
+});
+
+router.get("/admin", async (_req, res) => {
+    const header = _req.headers["authorization"];
+    if (!header || !header.startsWith("Bearer ")) {
+      return res.status(401).json({ erro: "token ausente" });
+    }
+    const token = header.slice(7);
+
+    const decode = jwt.decode(token);
+    try {
+        if (decode.papel != 0) {
+            return res.status(401).json({ erro: "você não tem permissão" })
+        }
+        else {
+            try {
+                const { rows } = await pool.query('SELECT * FROM "Mensagens" ORDER BY "id" DESC;');
+                return res.json(rows);
+            }
+            catch (erro) {
+                return res.status(500).json({ erro: "erro interno" });
+            }
+        }
+    }
+    catch (erro){
+        return res.status(500).json({erro: "erro interno"});
     }
 });
 
@@ -47,6 +80,9 @@ router.get("/", async (_req, res) => {
 //GET /api/mensagens/:id
 router.get("/:id", async (req, res) => {
     const id = Number(req.params.id);
+    const header = req.headers["authorization"];
+    const token = header.slice(7);
+    const decode = jwt.decode(token);
 
     if (!Number.isInteger(id) || id <= 0) {
         return res.status(400).json({ erro: "id inválido" });
@@ -57,7 +93,12 @@ router.get("/:id", async (req, res) => {
         const { rows } = result;
         if (!rows[0]) return res.status(404).json({ erro: "não encontrado" });
 
-        res.json(rows[0]);
+        //autentição(analisando se é o usuário que enviou, o destinatário ou administrador)
+        const {Usuarios_id, Usuarios_id_destinatario} = rows[0];
+        if(Usuarios_id == decode.sub || Usuarios_id_destinatario == decode.sub || decode.papel == 0) 
+            return res.json(rows[0]);
+        else
+            return res.status(404).json({ erro: "não encontrado!" });
     } catch {
         res.status(500).json({ erro: "erro interno" });
     }
@@ -72,13 +113,14 @@ router.get("/:id", async (req, res) => {
 
 //POST /api/mensagens
 router.post("/", async (req, res) => {
-    const { Usuarios_id, Usuarios_id_destinatario,  mensagem} = req.body ?? {};
+    const { Usuarios_id, Usuarios_id_destinatario, mensagem } = req.body ?? {};
 
     const uId = Number(Usuarios_id);
     const dId = Number(Usuarios_id_destinatario);
-    if (!mensagem || typeof(mensagem) != 'string' || uId == null || dId == null || Number.isNaN(uId) || Number.isNaN(dId) || uId < 1 || dId < 1
-    || uId == dId) {
-        return res.status(400).json({ erro: `Usuarios_id, Usuarios_id_destinatario(Number >= 0 && uId != dId) e
+    if (!mensagem || typeof (mensagem) != 'string' || uId == null || dId == null || Number.isNaN(uId) || Number.isNaN(dId) || uId < 1 || dId < 1
+        || uId == dId) {
+        return res.status(400).json({
+            erro: `Usuarios_id, Usuarios_id_destinatario(Number >= 0 && uId != dId) e
                 mensagem(tipo string e não vazio) são obrigatórios` });
     }
 
@@ -89,7 +131,7 @@ router.post("/", async (req, res) => {
         );
 
         res.status(201).json(rows[0]);
-    } catch(erro) {
+    } catch (erro) {
         console.log(erro);
         res.status(500).json({ erro: "erro interno" });
     }
@@ -105,17 +147,18 @@ router.post("/", async (req, res) => {
 // PUT /api/mensagens/:id
 router.put("/:id", async (req, res) => {
     const id = Number(req.params.id);
-    const { Usuarios_id, Usuarios_id_destinatario,  mensagem } = req.body ?? {};
+    const { Usuarios_id, Usuarios_id_destinatario, mensagem } = req.body ?? {};
     const uId = Number(Usuarios_id);
     const dId = Number(Usuarios_id_destinatario);
 
     if (!Number.isInteger(id) || id <= 0) {
         return res.status(400).json({ erro: "id inválido" });
     }
-    if (!mensagem || typeof(mensagem) != 'string' || uId == null || dId == null
-    || Number.isNaN(uId) || Number.isNaN(dId)
-    || uId < 1 || dId < 1 || uId == dId) {
-        return res.status(400).json({ erro: `usuarios_id, Usuarios_id_destinatario(Number >= 0 && uId != dId) e
+    if (!mensagem || typeof (mensagem) != 'string' || uId == null || dId == null
+        || Number.isNaN(uId) || Number.isNaN(dId)
+        || uId < 1 || dId < 1 || uId == dId) {
+        return res.status(400).json({
+            erro: `usuarios_id, Usuarios_id_destinatario(Number >= 0 && uId != dId) e
                 mensagem(tipo string e não vazio) são obrigatórios` });
     }
 
@@ -144,7 +187,7 @@ router.put("/:id", async (req, res) => {
 // /api/mensagens/:id
 router.patch("/:id", async (req, res) => {
     const id = Number(req.params.id);
-    const { Usuarios_id, Usuarios_id_destinatario,  mensagem } = req.body ?? {};
+    const { Usuarios_id, Usuarios_id_destinatario, mensagem } = req.body ?? {};
 
     const uId = Number(Usuarios_id);
     const dId = Number(Usuarios_id_destinatario);
@@ -164,9 +207,9 @@ router.patch("/:id", async (req, res) => {
         }
     }
 
-    if(Usuarios_id_destinatario !== undefined){
-        if( Number.isNaN(dId) || dId < 0){
-            return res.status(400).json({erro: "ids de destinatario devem ser número >= 0"})
+    if (Usuarios_id_destinatario !== undefined) {
+        if (Number.isNaN(dId) || dId < 0) {
+            return res.status(400).json({ erro: "ids de destinatario devem ser número >= 0" })
         }
     }
 
@@ -207,11 +250,5 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json({ erro: "erro interno" });
     }
 });
-
-
-
-
-
-
 
 export default router;
